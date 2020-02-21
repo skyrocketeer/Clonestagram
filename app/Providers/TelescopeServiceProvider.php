9e368857efac72c3338e04d91f1d28bf4ec7723a
jimmy <duylinh0147@outlook.com>
2020-02-22 03:06:05 +0700

    fix package not found

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -5,67 +5,67 @@ namespace App\Providers;
 use Laravel\Telescope\Telescope;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
-use Laravel\Telescope\TelescopeApplicationServiceProvider;
+// use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
-class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        // Telescope::night();
+// class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
+// {
+//     /**
+//      * Register any application services.
+//      *
+//      * @return void
+//      */
+//     public function register()
+//     {
+//         // Telescope::night();
 
-        $this->hideSensitiveRequestDetails();
+//         $this->hideSensitiveRequestDetails();
 
-        Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->isLocal()) {
-                return true;
-            }
+//         Telescope::filter(function (IncomingEntry $entry) {
+//             if ($this->app->isLocal()) {
+//                 return true;
+//             }
 
-            return $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
-        });
-    }
+//             return $entry->isReportableException() ||
+//                    $entry->isFailedRequest() ||
+//                    $entry->isFailedJob() ||
+//                    $entry->isScheduledTask() ||
+//                    $entry->hasMonitoredTag();
+//         });
+//     }
 
-    /**
-     * Prevent sensitive request details from being logged by Telescope.
-     *
-     * @return void
-     */
-    protected function hideSensitiveRequestDetails()
-    {
-        if ($this->app->isLocal()) {
-            return;
-        }
+//     /**
+//      * Prevent sensitive request details from being logged by Telescope.
+//      *
+//      * @return void
+//      */
+//     protected function hideSensitiveRequestDetails()
+//     {
+//         if ($this->app->isLocal()) {
+//             return;
+//         }
 
-        Telescope::hideRequestParameters(['_token']);
+//         Telescope::hideRequestParameters(['_token']);
 
-        Telescope::hideRequestHeaders([
-            'cookie',
-            'x-csrf-token',
-            'x-xsrf-token',
-        ]);
-    }
+//         Telescope::hideRequestHeaders([
+//             'cookie',
+//             'x-csrf-token',
+//             'x-xsrf-token',
+//         ]);
+//     }
 
-    /**
-     * Register the Telescope gate.
-     *
-     * This gate determines who can access Telescope in non-local environments.
-     *
-     * @return void
-     */
-    protected function gate()
-    {
-        Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
-        });
-    }
-}
+//     /**
+//      * Register the Telescope gate.
+//      *
+//      * This gate determines who can access Telescope in non-local environments.
+//      *
+//      * @return void
+//      */
+//     protected function gate()
+//     {
+//         Gate::define('viewTelescope', function ($user) {
+//             return in_array($user->email, [
+//                 //
+//             ]);
+//         });
+//     }
+// }
